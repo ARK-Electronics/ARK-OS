5bccb713854dd84e207ddbf5d38a56fefc213596
Jacob Dahl <dahl.jakejacob@gmail.com>
2025-04-10 21:19:58 -0800

    print more

--- platform/jetson/scripts/test_jtop.py
+++ platform/jetson/scripts/test_jtop.py
@@ -12,3 +12,4 @@ with jtop() as jetson:
         # Convert datetime/timedelta to string, then pretty-print
         stats = {k: convert(v) for k, v in jetson.stats.items()}
         print(json.dumps(stats, indent=4))
+        print(jetson.stats)
