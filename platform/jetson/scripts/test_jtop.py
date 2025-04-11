c4b1521ef166a8b9e90dd039816e4329f2318329
Jacob Dahl <dahl.jakejacob@gmail.com>
2025-04-10 22:15:39 -0800

    all

--- platform/jetson/scripts/test_jtop.py
+++ platform/jetson/scripts/test_jtop.py
@@ -2,6 +2,16 @@ from jtop import jtop
 
 with jtop() as jetson:
     while jetson.ok():
-        # print(jetson.stats)
+        print(jetson.json)
         print(jetson.power)
+        print(jetson.temperature)
+        print(jetson.local_interfaces)
+        print(jetson.board)
+        print(jetson.nvpmodel)
+        print(jetson.jetson_clocks)
+
+
+
+
+
 
