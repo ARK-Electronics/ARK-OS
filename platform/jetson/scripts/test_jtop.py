247ffb7dace09c9ce5bf611e98efd67ef19bf3ed
Jacob Dahl <dahl.jakejacob@gmail.com>
2025-04-10 22:13:03 -0800

    power

--- platform/jetson/scripts/test_jtop.py
+++ platform/jetson/scripts/test_jtop.py
@@ -1,15 +1,7 @@
 from jtop import jtop
-import json
-from datetime import datetime, timedelta
-
-def convert(obj):
-    if isinstance(obj, (datetime, timedelta)):
-        return str(obj)
-    return obj
 
 with jtop() as jetson:
     while jetson.ok():
-        # Convert datetime/timedelta to string, then pretty-print
-        stats = {k: convert(v) for k, v in jetson.stats.items()}
-        print(json.dumps(stats, indent=4))
-        print(jetson.stats)
+        # print(jetson.stats)
+        print(jetson.power)
+
