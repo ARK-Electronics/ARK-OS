ea9212c2d88ae32c95299e8086b89220dacb3aef
Jacob Dahl <dahl.jakejacob@gmail.com>
2025-04-10 22:18:59 -0800

    spacing

--- platform/jetson/scripts/test_jtop.py
+++ platform/jetson/scripts/test_jtop.py
@@ -3,10 +3,15 @@ from jtop import jtop
 with jtop() as jetson:
     while jetson.ok():
         print(jetson.power)
+        print("\n")
         print(jetson.temperature)
+        print("\n")
         print(jetson.local_interfaces)
+        print("\n")
         print(jetson.board)
+        print("\n")
         print(jetson.nvpmodel)
+        print("\n")
         print(jetson.jetson_clocks)
 
 
