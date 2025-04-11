274e88cc93f19f231479ae75b918e8c4b2968fbe
Jacob Dahl <dahl.jakejacob@gmail.com>
2025-04-10 21:16:45 -0800

    json

--- platform/jetson/scripts/test_jtop.py
+++ platform/jetson/scripts/test_jtop.py
@@ -1,7 +1,14 @@
 from jtop import jtop
+import json
+from datetime import datetime, timedelta
+
+def convert(obj):
+    if isinstance(obj, (datetime, timedelta)):
+        return str(obj)
+    return obj
 
 with jtop() as jetson:
-    # jetson.ok() will provide the proper update frequency
     while jetson.ok():
-        # Read tegra stats
-        print(jetson.stats)
+        # Convert datetime/timedelta to string, then pretty-print
+        stats = {k: convert(v) for k, v in jetson.stats.items()}
+        print(json.dumps(stats, indent=4))
